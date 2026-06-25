8a19bcf44c48a016d77bfd00851a3d29894e0591
micktomi <newguitar67@gmail.com>
2026-06-25 14:11:45 +0300

    Restore stable mobile Filament theme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,8 +33,8 @@ class AdminPanelProvider extends PanelProvider
             ->login()
             ->brandName('Διαχείριση Συνεργείου')
             ->colors([
-                'primary' => Color::Amber,
-            ])
+    'primary' => Color::Slate,
+])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
